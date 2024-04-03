c7b8b069108da41f183c643b1f3fbc8e76475be5
Olekrr <93515165+Olekrr@users.noreply.github.com>
2024-04-03 22:54:01 +0200

    created Contact page

--- react-ca/src/App.js
+++ react-ca/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/Homepage/Homepage';
 import Layout from './components/Layout/Layout';
+import ContactPage from './components/ContactPage/ContactPage';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Layout>
   );
---
+++ react-ca/src/components/ContactPage/ContactPage.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import "./ContactPage.scss";
+
+const ContactPage = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    subject: "",
+    email: "",
+    body: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      Object.values(formData).every((field) => field.trim().length >= 3) &&
+      /\S+@\S+\.\S+/.test(formData.email)
+    ) {
+      console.log(formData);
+      setFormData({
+        fullName: "",
+        subject: "",
+        email: "",
+        body: "",
+      });
+    } else {
+      alert("Please fill out the form correctly.");
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2>Contact Us</h2>
+      <form onSubmit={handleSubmit} className="mt-4 contact-form">
+        <div className="mb-3">
+          <label htmlFor="fullName" className="form-label">
+            Full Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="fullName"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            required
+            minLength={3}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="subject" className="form-label">
+            Subject
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="subject"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            required
+            minLength={3}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="body" className="form-label">
+            Message
+          </label>
+          <textarea
+            className="form-control"
+            id="body"
+            name="body"
+            rows="3"
+            value={formData.body}
+            onChange={handleChange}
+            required
+            minLength={3}
+          ></textarea>
+        </div>
+        <div className="button-container">
+          <button type="submit" className="btn btn-primary custom-button-width">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactPage;
