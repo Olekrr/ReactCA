8f6d5b6288969a9f0cebbf06a1f62a7d89799820
Olekrr <93515165+Olekrr@users.noreply.github.com>
2024-04-07 03:12:52 +0200

    added user feedback for adding items to cart

--- react-ca/src/components/ProductPage/ProductPage.jsx
+++ react-ca/src/components/ProductPage/ProductPage.jsx
@@ -9,6 +9,7 @@ const ProductPage = () => {
   const [product, setProduct] = useState(null);
   const [error, setError] = useState(null);
   const { addToCart } = useCart();
+  const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
     const getProduct = async () => {
@@ -29,6 +30,8 @@ const ProductPage = () => {
 
   const handleAddToCart = () => {
     addToCart(product);
+    setShowNotification(true);
+    setTimeout(() => setShowNotification(false), 3000);
   };
 
   if (error) {
@@ -45,31 +48,40 @@ const ProductPage = () => {
     ? ((product.price - product.discountedPrice) / product.price) * 100
     : 0;
 
-    return (
-      <div className="product-page-container">
-        <h2>{product.title}</h2>
-        <img src={imageUrl} alt={product.title} />
-        <p>{product.description}</p>
-        <p>
-          Price: ${product.discountedPrice}
-          {hasDiscount && (
-            <span> (Discount: {discountPercentage.toFixed(2)}%, Original Price: ${product.price})</span>
-          )}
-        </p>
-        <button onClick={handleAddToCart}>Add to Cart</button>
-        {product.reviews && product.reviews.length > 0 && (
-          <div className="reviews-section">
-            <h3>Reviews</h3>
-            {product.reviews.map((review) => (
-              <div key={review.id}>
-                <p><strong>{review.username}</strong>: {review.description} - {review.rating} Stars</p>
-              </div>
-            ))}
-          </div>
+  return (
+    <div className="product-page-container">
+      <h2>{product.title}</h2>
+      <img src={imageUrl} alt={product.title} />
+      <p>{product.description}</p>
+      <p>
+        Price: ${product.discountedPrice}
+        {hasDiscount && (
+          <span>
+            {" "}
+            (Discount: {discountPercentage.toFixed(2)}%, Original Price: $
+            {product.price})
+          </span>
         )}
-      </div>
-    );
-    
+      </p>
+      <button onClick={handleAddToCart}>Add to Cart</button>
+      {showNotification && (
+        <div className="notification">Added "{product.title}" to cart!</div>
+      )}
+      {product.reviews && product.reviews.length > 0 && (
+        <div className="reviews-section">
+          <h3>Reviews</h3>
+          {product.reviews.map((review) => (
+            <div key={review.id}>
+              <p>
+                <strong>{review.username}</strong>: {review.description} -{" "}
+                {review.rating} Stars
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ProductPage;
